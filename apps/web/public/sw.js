b49717de259ad901fe0961e14837b2bfb34ea37a
Rihar <Rihar@DELLPROMAX16>
2026-06-27 18:07:24 +0200

    chore: bump version to 4.6

--- apps/web/public/sw.js
+++ apps/web/public/sw.js
@@ -13,7 +13,7 @@
    WebSocket / cita-domēna pieprasījumi netiek skarti. `activate` izdzēš VISUS
    iepriekšējo versiju kešus, tāpēc CACHE versija jāpalielina laidienos, kas maina
    kešotos aktīvus (statisks SW nevar pats atvasināt build id). */
-const CACHE = "domino-poker-v4.5";
+const CACHE = "domino-poker-v4.6";
 const PRECACHE = ["/", "/manifest.webmanifest", "/icons/icon-192.png", "/icons/icon-512.png"];
 
 // Novērojamība (m12): kešošanas kļūdas paliek NE-fatālas (SW nedrīkst lauzt lapu),
